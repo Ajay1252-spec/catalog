47d52e024c8c19ecc4f28ea7a07d22ab6ebe06fe
sri ram Kunamsetty <pasupuletiajaykumar1252@gmail.com>
2025-03-31 00:55:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChartTabPage from './components/ChartTabPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ChartTabPage />
+    </div>
+  );
+}
+
+export default App;
